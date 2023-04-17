38f96519690cd947e603183b42a98e058b71bae5
Adam Preston <99239630+acpresto@users.noreply.github.com>
2023-04-16 20:46:46 -0400

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,44 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "1d192d40-03ea-4a97-bc4f-d510cc725bba",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def hours():\n",
+    "    print(\"Open 9-5 daily\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "52cf9e2b-cd43-4bd5-bf30-2e18eccfcc08",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
